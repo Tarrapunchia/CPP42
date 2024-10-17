5e3ccc7d978b1a860e906e4633206582f401c2ff
tarrapunchia <f.zuc87@gmail.com>
2024-10-17 23:05:10 +0200

    CPP_01 - ex05 - color is life

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -18,10 +18,10 @@ void Harl::complain(std::string level)
     };
 
     std::string levels[4] = {
-        "debug",
-        "info",
-        "warning",
-        "error"
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR"
     };
     
     for (size_t i = 0; i < 4; i++)
@@ -38,20 +38,20 @@ void Harl::complain(std::string level)
 
 void Harl::debug() const
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+    std::cout << "\033[1;37m[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\033[0m" << std::endl;
 }
 
 void Harl::info() const
 {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+    std::cout << "\033[1;34m[INFO]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\033[0m" << std::endl;
 }
 
 void Harl::warning() const
 {
-    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "\033[1;33m[WARNING]\nI think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\033[0m" << std::endl;
 }
 
 void Harl::error() const
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << "\033[1;31m[ERROR]\nThis is unacceptable! I want to speak to the manager now.\033[0m" << std::endl;
 }
--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -4,14 +4,14 @@ int main(int argc, char const *argv[])
 {
     Harl    harl;
 
-    std::cout << "Hi, how can I help you with DEBUGGING, Sir?" << std::endl;
-    harl.complain("debug");
-    std::cout << "Sir you've been given the wrong INFO..." << std::endl;
-    harl.complain("info");
-    std::cout << "You don't understand: your WARNINGs are useless since we're at..." << std::endl;
-    harl.complain("warning");
-    std::cout << "Sir, listen to me: there's been an ERROR..." << std::endl;
-    harl.complain("error");
-    std::cout << "Sir you're actually @42Florence right now..." << std::endl;
+    std::cout << "[MAIN]\nHi, how can I help you with DEBUGGING, Sir?" << std::endl;
+    harl.complain("DEBUG");
+    std::cout << "[MAIN]\nSir you've been given the wrong INFO..." << std::endl;
+    harl.complain("INFO");
+    std::cout << "[MAIN]\nYou don't understand: your WARNINGs are useless since we're at..." << std::endl;
+    harl.complain("WARNING");
+    std::cout << "[MAIN]\nSir, listen to me: there's been an ERROR..." << std::endl;
+    harl.complain("ERROR");
+    std::cout << "[MAIN]\nSir you're actually @42Florence right now..." << std::endl;
     return 0;
 }
