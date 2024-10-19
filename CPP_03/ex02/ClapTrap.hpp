51eca9ce7cc381bfd6b4728328f0dd01b59b12dc
tarrapunchia <f.zuc87@gmail.com>
2024-10-19 13:40:30 +0200

    CPP_03 - ex02 done

---
+++ CPP_03/ex02/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAP_TRAP_HPP
+# define CLAP_TRAP_HPP
+# include <iostream>
+
+class ClapTrap
+{
+protected:
+    std::string         _name;
+    int                 _hitPoints;
+    int                 _energyPoints;
+    int                 _attackDamage;
+public:
+    ClapTrap( void );
+    ClapTrap( std::string name );
+    ClapTrap( const ClapTrap& other );
+    ClapTrap &operator=( const ClapTrap& e );
+    ~ClapTrap( void );
+
+    void    attack( const std::string& target );
+    void    takeDamage( unsigned int amount );
+    void    beRepaired( unsigned int amount );
+};
+
+
+#endif
---
+++ CPP_03/ex02/FragTrap.hpp
@@ -0,0 +1,18 @@
+#ifndef FRAG_TRAP_HPP
+# define FRAG_TRAP_HPP
+# include "ClapTrap.hpp"
+
+class FragTrap: public ClapTrap
+{
+private:
+
+public:
+    FragTrap( void );
+    FragTrap( const FragTrap& other );
+    FragTrap &operator=( const FragTrap& e );
+    ~FragTrap( void );
+
+    void highFiveGuys( void ) const;
+};
+
+#endif
